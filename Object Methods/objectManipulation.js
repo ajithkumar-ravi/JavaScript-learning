8c18990aa5000a516557a98d7ee03eff382ce8e1
ajithkumar-ravi <itsajith005@gmail.com>
2025-09-23 15:39:18 +0530

    Add: Added some methods

--- Object Methods/objectManipulation.js
+++ Object Methods/objectManipulation.js
@@ -6,6 +6,32 @@ let person = {
 }
 
 Object.assign(person, { Dept: "CSE", Degree: "BE", Percentage: 80})
-console.log(person);
+// console.log(person);
 
+
+//---------------Object Entries
+// Object.entries is used to convert object into arr
+
+let employee = {
+    eName: "Ak",
+    eRoll : "Backend Dev"
+}
+
+console.log(employee);
+let multiArr = Object.entries(employee)
+let multiArrVal = console.log(multiArr);
+
+
+//---------------Object FromEntries------------
+// FromEntries is used for converting array into object
+
+let narmalObj = Object.fromEntries(multiArr)
+console.log(narmalObj);
+
+
+//---------------Object.keys------------
+//  It is used for separating keys only
+
+let onlyKeys = Object.keys(person)
+console.log(onlyKeys);
 
