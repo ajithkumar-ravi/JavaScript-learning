3066e482130e648fd67414806f22d2bd518bb19c
ajithkumar-ravi <itsajith005@gmail.com>
2025-09-23 14:47:51 +0530

    Add: Added Object assing

---
+++ Object Methods/objectManipulation.js
@@ -0,0 +1,9 @@
+//-------------------Object assign--------------------
+
+let person = {
+    Name: "ajith",
+    age: "20"
+}
+
+Object.assign(person, { Dept: "CSE", Degree: "BE"})
+console.log(person);
