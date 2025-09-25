70c44d97b7db3a800d57b0b4301e94c4ea6a0575
ajithkumar-ravi <itsajith005@gmail.com>
2025-09-25 14:53:16 +0530

    Add: Added async and await basics

--- Asynchronous JavaScript/fetchApi.js
+++ Asynchronous JavaScript/fetchApi.js
@@ -3,37 +3,30 @@ fetch("https://fakestoreapi.com/products/1")
     if (!response.ok) {
       throw new Error("Data is not Recieved");
     }
-    else{
-      console.log(response.ok); // ok property only return true and false value
-      return response.json();
-    }
+    console.log(response.ok); // ok property only return true and false value
+    return response.json();
   })
   .then((data) => {
     console.log(data);
   })
   .catch((error) => {
     console.log("Data is not Recieved:", error.message);
-  })
+  });
 
-// console.log(fetchData);
+//-----------using async await-----------------
 
+async function fetchData() {
+  try {
+    let response = await fetch("https://fakestoreapi.com/products/1");
 
+    if (!response.ok) {
+      throw new Error("DATA IS NOT RECIEVED");
+    }
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    error.message;
+  }
+}
 
-
-//------------------------
-
-//  .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Data is not Recieved");
-//     } 
-//     else {
-//       console.log(response.ok); // ok property only return true and false value
-//       return response.json().
-//       then((data) => console.log(data)).
-//       catch((error) => {
-//         console.log(error.message)
-//     });
-
-//     }
-
-//   })
+fetchData();
